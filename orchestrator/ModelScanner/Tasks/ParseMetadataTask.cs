90dcc1af7704f81161b743775bd6d7fd0e3b6406
Koen <koen@linker.io>
2024-08-16 23:54:59 +0100

    Parse metadata best effort

--- orchestrator/ModelScanner/Tasks/ParseMetadataTask.cs
+++ orchestrator/ModelScanner/Tasks/ParseMetadataTask.cs
@@ -2,39 +2,49 @@
 
 namespace ModelScanner.Tasks;
 
-public class ParseMetadataTask: IJobTask
+public class ParseMetadataTask : IJobTask
 {
-  readonly ILogger<ParseMetadataTask> _logger;
+    readonly ILogger<ParseMetadataTask> _logger;
 
-  public ParseMetadataTask(ILogger<ParseMetadataTask> logger)
-  {
-      _logger = logger;
-  }
-
-  public JobTaskTypes TaskType => JobTaskTypes.ParseMetadata;
-
-  public Task<bool> Process(string filePath, ScanResult result, CancellationToken cancellationToken)
-  {
-    var fileExtension = Path.GetExtension(filePath);
-
-    if (fileExtension != ".safetensors") {
-      _logger.LogInformation("Skipping parsing of metadata for {type} files. Safetensor file required", fileExtension);
-      // Bail early but return true to continue with additional tasks
-      return Task.FromResult(true);
+    public ParseMetadataTask(ILogger<ParseMetadataTask> logger)
+    {
+        _logger = logger;
     }
 
-    using var fileStream = File.OpenRead(filePath);
-    var buffer = new byte[8];
-    fileStream.Position = 0;
-    fileStream.ReadExactly(buffer);
-    var metadataLength = BitConverter.ToUInt64(buffer);
-    var metadataBuffer = new byte[metadataLength];
-    fileStream.ReadExactly(metadataBuffer);
+    public JobTaskTypes TaskType => JobTaskTypes.ParseMetadata;
 
-    var metadataJson = JsonSerializer.Deserialize<JsonDocument>(metadataBuffer);
-    result.Metadata = metadataJson;
+    public Task<bool> Process(string filePath, ScanResult result, CancellationToken cancellationToken)
+    {
+        try
+        {
 
-    return Task.FromResult(true);
-  }
+            var fileExtension = Path.GetExtension(filePath);
+
+            if (fileExtension != ".safetensors")
+            {
+                _logger.LogInformation("Skipping parsing of metadata for {type} files. Safetensor file required", fileExtension);
+                // Bail early but return true to continue with additional tasks
+                return Task.FromResult(true);
+            }
+
+            using var fileStream = File.OpenRead(filePath);
+            var buffer = new byte[8];
+            fileStream.Position = 0;
+            fileStream.ReadExactly(buffer);
+            var metadataLength = BitConverter.ToUInt64(buffer);
+            var metadataBuffer = new byte[metadataLength];
+            fileStream.ReadExactly(metadataBuffer);
+
+            var metadataJson = JsonSerializer.Deserialize<JsonDocument>(metadataBuffer);
+            result.Metadata = metadataJson;
+
+            return Task.FromResult(true);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "ParseMetadata failed. Ignore exception as this is a best effort only");
+            return Task.FromResult(true);
+        }
+    }
 
 }
